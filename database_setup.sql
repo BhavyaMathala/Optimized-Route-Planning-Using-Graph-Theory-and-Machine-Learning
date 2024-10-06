c2bf15b026f9399f8cc593471f352086ac43c207
Mathala Bhavya <143822365+BhavyaMathala@users.noreply.github.com>
2024-10-06 18:56:39 +0530

    database_setup.sql

--- database_setup.sql
+++ database_setup.sql
@@ -1,17 +1,42 @@
--- Create the routes table
-CREATE TABLE IF NOT EXISTS routes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    city1 VARCHAR(255),
-    city2 VARCHAR(255),
-    distance INT,
-    traffic VARCHAR(50),
-    weather VARCHAR(50)
+CREATE TABLE traffic_data (
+    id INT,
+    segment_id INT,
+    roadway_name VARCHAR(100),
+    from_location VARCHAR(100),
+    to_location VARCHAR(100),
+    direction VARCHAR(10),
+    date DATE,
+    hour_12_1AM INT,
+    hour_1_2AM INT,
+    hour_2_3AM INT,
+    hour_3_4AM INT,
+    hour_4_5AM INT,
+    hour_5_6AM INT,
+    hour_6_7AM INT,
+    hour_7_8AM INT,
+    hour_8_9AM INT,
+    hour_9_10AM INT,
+    hour_10_11AM INT,
+    hour_11_12PM INT,
+    hour_12_1PM INT,
+    hour_1_2PM INT,
+    hour_2_3PM INT,
+    hour_3_4PM INT,
+    hour_4_5PM INT,
+    hour_5_6PM INT,
+    hour_6_7PM INT,
+    hour_7_8PM INT,
+    hour_8_9PM INT,
+    hour_9_10PM INT,
+    hour_10_11PM INT,
+    hour_11_12AM INT
 );
 
--- Load data into the routes table
-LOAD DATA INFILE '/path/to/your/data/route_data.csv'
-INTO TABLE routes
+LOAD DATA INFILE 'path.csv'
+INTO TABLE traffic_data
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(city1, city2, distance, traffic, weather);
+IGNORE 1 LINES;
+
+
+
